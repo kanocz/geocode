45eb2445cf5a5ddeb4f91c4aec0f8dcb834055d6
Anton Skorokhod <anton@nsl.cz>
2016-03-14 11:30:27 +0100

    Use HTTPS instead of HTTP

--- geocode.go
+++ geocode.go
@@ -12,7 +12,7 @@ import (
 	"strconv"
 )
 
-const api = "http://maps.googleapis.com/maps/api/geocode/json"
+const api = "https://maps.googleapis.com/maps/api/geocode/json"
 
 type Request struct {
 	// One (and only one) of these must be set.
